7df4823c4e84bd40ee3535e67469690efa31791d
Oksana <ksenia79460@gmail.com>
2021-12-03 18:44:19 +0000

    delete import axios as unused

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import logo from "./logo.svg";
 import "./App.css";
 import Loader from "react-loader-spinner";
-import axios from "axios";
+
 import SearchCity from "./SearchCity";
 
 function App() {
