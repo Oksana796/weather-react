4c05218d76475dff77dea10dbdbc08df07c01400
Oksana <ksenia79460@gmail.com>
2021-12-03 16:47:43 +0000

    add component's style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import Loader from "react-loader-spinner";
 import axios from "axios";
--- src/SearchCity.js
+++ src/SearchCity.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-//import "./styles.css";
+import "./SearchCity.css";
 
 export default function SearchCity(props) {
   let [city, setCity] = useState(null);
